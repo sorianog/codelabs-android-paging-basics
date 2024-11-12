be87bdbb8a9f01a5995ab78bd8bd092dc644aff1
sorianog <13261711+sorianog@users.noreply.github.com>
2024-11-12 11:09:09 -0800

    Completed Step 5: Define the source of data

---
+++ app/src/main/java/com/example/android/codelabs/paging/data/ArticlePagingSource.kt
@@ -0,0 +1,50 @@
+package com.example.android.codelabs.paging.data
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import java.time.LocalDateTime
+import kotlin.math.max
+
+const val STARTING_KEY = 0
+private val firstArticleCreatedTime = LocalDateTime.now()
+
+class ArticlePagingSource : PagingSource<Int, Article>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
+        // Start paging with the STARTING_KEY if this is the first load
+        val start = params.key ?: STARTING_KEY
+        // Load as many items as hinted by params.loadSize
+        val range = start.until(start + params.loadSize)
+
+        return LoadResult.Page(
+            data = range.map { number ->
+                Article(
+                    id = number,
+                    title = "Article #: $number",
+                    description = "Here is the description for Article #: $number",
+                    created = firstArticleCreatedTime.minusDays(number.toLong())
+                )
+            },
+            // Make sure we don't try to load items behind the STARTING_KEY
+            prevKey = when (start) {
+                STARTING_KEY -> null
+                else -> { ensureValidKey(range.first - params.loadSize) }
+            },
+            nextKey = range.last + 1
+        )
+    }
+
+    // The refresh key is used for the initial load of the next PagingSource, after invalidation
+    override fun getRefreshKey(state: PagingState<Int, Article>): Int? {
+        // In our case we grab the item closest to the anchor position
+        // then return its id - (state.config.pageSize / 2) as a buffer
+        val anchorPosition = state.anchorPosition ?: return null
+        val article = state.closestItemToPosition(anchorPosition) ?: return null
+        ensureValidKey(article.id - (state.config.pageSize / 2))
+    }
+
+    /**
+     * Makes sure the paging key is never less than [STARTING_KEY]
+     */
+    private fun ensureValidKey(key: Int) = max(STARTING_KEY, key)
+}
